ef2b6f3f33dcb4958a9532633f9c7284163c5f90
Stratos <stratouklos@gmail.com>
2012-12-24 06:57:35 -0500

    Move the resources to the right place

---
+++ src/com/nullpointerengineering/android/pomodoro/activities/TaskManager.java
@@ -0,0 +1,64 @@
+package com.nullpointerengineering.android.pomodoro.activities;
+
+
+import android.os.Bundle;
+import com.example.Pomodoro.R;
+import com.google.inject.Inject;
+import com.nullpointerengineering.android.pomodoro.utilities.Eula;
+import roboguice.activity.RoboActivity;
+
+public class TaskManager extends RoboActivity {
+
+    @Inject
+    Eula eula;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pomodoro);
+
+        eula.show(this);
+
+//        ListView list = getListView();
+//        registerForContextMenu(list);
+ //       list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+ //           @Override
+ //           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+ //               editTaskActivity((Long) view.getTag());
+/*
+            }
+        });
+
+        //On clickers
+        ( findViewById(R.id.PlayBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO implement
+            }
+        });
+
+        ( findViewById(R.id.AddTaskBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTaskActivity(0);
+            }
+        });
+
+        ( findViewById(R.id.PreferenceBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO implement
+            }
+        });
+
+        ( findViewById(R.id.StatisticsBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO implement
+            }
+        });
+
+*/
+    }
+
+}
