885e35137db33f3058bc5355e022dfac91507683
Stratos <stratouklos@gmail.com>
2013-07-13 23:02:08 -0400

    Moving the EULA

--- src/com/nullpointerengineering/android/pomodoro/eula/Eula.java
+++ src/com/nullpointerengineering/android/pomodoro/eula/Eula.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.nullpointerengineering.android.pomodoro.utilities;
+package com.nullpointerengineering.android.pomodoro.eula;
 
 import android.app.Activity;
 import android.app.AlertDialog;
